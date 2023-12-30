127b41e73e1895ba30a697f04787cf3af67ec2e5
lknt <22251279@zju.edu.cn>
2023-12-30 22:55:59 +0800

    submodule

--- 7_ExternalGit/app/main.cc
+++ 7_ExternalGit/app/main.cc
@@ -1,12 +1,19 @@
 #include <iostream>
 
+#include "nlohmann/json.hpp"
 #include "my_lib.h"
 #include "config.hpp"
+
+
 
 int main()
 {
     std::cout << project_name << "\n";
     std::cout << project_version << "\n";
+
+    std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "." 
+        << NLOHMANN_JSON_VERSION_MINOR << "." 
+        << NLOHMANN_JSON_VERSION_PATCH << std::endl;
     print_hello_world();
     return 0;
 }
---
+++ 7_ExternalGit/cmake/AddGitSubmodule.cmake
@@ -0,0 +1,15 @@
+function(add_git_submodule dir)
+    find_package(Git REQUIRED)
+
+    if (NOT EXISTS ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt)
+        execute_process(COMMAND ${GIT_EXECUTABLE}
+            submodule update --init --recursive -- ${CMAKE_SOURCE_DIR}/${dir}
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+    endif()
+    if (EXISTS ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt)
+        add_subdirectory(${CMAKE_SOURCE_DIR}/${dir})
+    endif()
+    
+endfunction(add_git_submodule)
+    
+        
